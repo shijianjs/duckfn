26811585eb67299bec5ee5c131652445abf4af64
shijianjs <shijianjs@hotmail.com>
2026-08-10 15:52:17 +0800

    refactor(duck_args_type): 移除不必要的常量定义
    - 删除 DuckArgs trait 中的 COUNT 常量
    - 删除元组类型实现中的 COUNT 常量定义
    - 简化代码结构，移除冗余的常量声明

--- src/duck_args_type.rs
+++ src/duck_args_type.rs
@@ -2,7 +2,6 @@ use crate::duck_value_type_convertor::DuckValueType;
 use quack_rs::prelude::{TypeId, VectorReader};
 
 pub trait DuckArgs : Sized{
-    const COUNT: usize;
 
     fn read(readers: &[VectorReader], row: usize) -> Self;
 
@@ -10,7 +9,6 @@ pub trait DuckArgs : Sized{
 }
 
 impl<A: DuckValueType> DuckArgs for (Option<A>,) {
-    const COUNT: usize = 1;
 
     fn read(readers: &[VectorReader], row: usize) -> Self {
         (A::read(&readers[0], row),)
@@ -22,7 +20,6 @@ impl<A: DuckValueType> DuckArgs for (Option<A>,) {
 }
 
 impl<A: DuckValueType, B: DuckValueType> DuckArgs for (Option<A>, Option<B>) {
-    const COUNT: usize = 2;
 
     fn read(readers: &[VectorReader], row: usize) -> Self {
         (A::read(&readers[0], row), B::read(&readers[1], row))
